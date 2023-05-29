a3772463a42db88fbe3bc5d57d40e191c399a466
carlos dominciano <carlos.dominciano@bandtec.com.br>
2023-05-28 21:38:36 -0300

    feat: added cardPlaylist component and css for it

--- web/src/components/cardPlaylist/CardPlaylist.jsx
+++ web/src/components/cardPlaylist/CardPlaylist.jsx
@@ -1,14 +1,33 @@
 import './cardPlaylist.css';
 import '../../css/root.css'
+import { useState } from 'react';
+import ModalPlaylist from '../modalPlaylist/ModalPlaylist.jsx';
 
 function CardPlaylist(props) {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    function handleOpenModal() {
+        setIsModalOpen(prev => !prev)
+    }
+
     return (
-        <section className="CardPlaylist">
-            <div className="container">
-                <h1>{props.name}</h1> <br />
-                <h1>{props.description}</h1> <br />
-                <h1>{props.musics}</h1> <br />
+        <section className="card-playlist">
+            <div className="container-card-playlist">
+                <button className="delete-playlist"
+                onClick={ () => props.functionDeletePlaylist(props.name)}
+                >X</button>
+                <h1>Name: {props.name}</h1> 
+                <h1>Description: {props.description}</h1>
+                <h1>Musics: {props.musics.length}</h1>
+                <button onClick={handleOpenModal}>See songs</button>
             </div>
+            {isModalOpen && 
+            <ModalPlaylist
+            name={props.name}
+            musics={props.musics}
+            functionHandleOpenModal={handleOpenModal}
+            />}
         </section>
     )
 }
